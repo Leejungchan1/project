14eb866c5652e0dfb81c22d09004a7221f524ee8
Leejungchan1 <wjdcks0123@naver.com>
2024-08-01 17:49:56 +0900

    프로잭트1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".left_move").slick({
+        autoplay:true,
+        autoplaySpeed:1500,
+        slidesToShow:2,
+        dots:true,
+        arrows:false,
+    })
+    
+})
