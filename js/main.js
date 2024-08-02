8de5c75270b6122c34dd2c730e65e9563896d2aa
Leejungchan1 <wjdcks0123@naver.com>
2024-08-02 17:48:16 +0900

    수정본

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $(function(){
     $(".left_move").slick({
         autoplay:true,
-        autoplaySpeed:1500,
+        autoplaySpeed:2000,
         slidesToShow:2,
         dots:true,
         arrows:false,
     })
+
+    $(".fav_move").slick({
+        autoplay:true,
+        autoplaySpeed:1500,
+        slidesToShow:4,
+        dots:false,
+        arrows:false,
+        centerMode:true,
+    })
+
     
 })
