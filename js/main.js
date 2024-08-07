ccf4166b1a9e9d2cb6799efec8853869b81faa29
Leejungchan1 <wjdcks0123@naver.com>
2024-08-07 17:24:47 +0900

    최종본

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    //pc 메뉴
     $(".main > li").hover(function(){
         $(this).children(".sub").css({opacity: 0, display: 'flex'}).animate({
             opacity: 1
@@ -13,7 +15,36 @@ $(function () {
         $(".sub_in").stop().slideUp("slow");
     })
 
+    
+    //m 메뉴
+    $(".menu-toggle-btn").click(function(){
+        $(this).toggleClass("on")
+        $(".m_main").slideToggle();
+    })
 
+    $(".m_main > li").hover(function(){
+        $(this).children(".m_sub").css({opacity: 0, display: 'flex'}).animate({
+            opacity: 1
+        }, 500);
+    },function(){
+        $(".m_sub").fadeOut(200);
+    })
+
+    $(".m_sub > li").hover(function(){
+        $(this).children(".m_sub_in").stop().slideDown("slow");
+    },function(){
+        $(".m_sub_in").stop().slideUp("slow");
+    })
+
+
+
+
+
+
+
+
+
+    //메인슬라이드
     $(".left_move").slick({
         autoplay: true,
         autoplaySpeed: 2500,
@@ -30,8 +61,14 @@ $(function () {
             ]
     })
 
-    // })
 
+
+
+
+    
+
+
+    //인기 상품
     $(".fav_move").slick({
         autoplay:true,
         autoplaySpeed:2000,
@@ -46,17 +83,28 @@ $(function () {
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                centerPadding: "100px",
+                centerPadding: "105px",
                 }
             }
         ]
 
     })
 
+
+
+
+    //팝업
     $(".close").click(function(){
         $(".popup").hide()
     })
 
+
+    
+
+    //a태그 이벤트 중지
+    $("a").click(function(event){
+        event.preventDefault();
+    })
 
 
 
